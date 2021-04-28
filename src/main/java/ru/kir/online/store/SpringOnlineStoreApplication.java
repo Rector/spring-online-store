db7533dd3984c1f26b727d825dd3d7a1f35a4958
kirill <kirill261989@mail.ru>
2021-04-28 19:17:24 +0400

    Initial commit

---
+++ src/main/java/ru/kir/online/store/SpringOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.kir.online.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOnlineStoreApplication.class, args);
+	}
+
+}
